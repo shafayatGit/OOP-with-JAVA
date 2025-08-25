62c4ed23a6a4aca3c054c8ac2ec0780afbf7682f
shafayatGit <shafayathossain.ielts@gmail.com>
2025-08-25 19:57:50 +0600

    push

--- src/OutputsMID.java
+++ src/OutputsMID.java
@@ -1,55 +1,36 @@
-
-class Sum {
-    int x, y, z;
-
-    {
-        System.out.println(x + y + z);
-    }
+class Person {
+    int id;
+    String name;
+    static int s = 10;
 
     {
-        x = 11;
-        y = 22;
-        z = 33;
-        System.out.println(x + y + z);
+        System.out.println("3");
     }
 
-    public Sum(int x) {
-        this.x = x;
-        System.out.println(x + y + z);
+    public Person() {
+        this.id = 1;
+        this.name = "M";
+        System.out.println("1");
+        s++;
     }
 
-    public Sum(int x, int y) {
-        this(500);
-        this.x = x;
-        this.y = y;
-        System.out.println(x + y + z);
+    public Person(int id, String name) {
+        this();
+        this.id = id;
+        this.name = name;
+        System.out.println("2");
+        s++;
     }
-
-    public Sum(int x, int y, int z) {
-        this(50, 20);
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        System.out.println(x + y + z);
-
-    }
-
-
-    public void display() {
-        this.display("The pen is stronger than the sword.");
-    }
-
-    public void display(String str) {
-        System.out.println(str);
-    }
-
 }
 
 
 public class OutputsMID {
     public static void main(String args[]) {
-        Sum s = new Sum(100, 150, 300);
-        s.display();
+        Person p = new Person(1, "N");
+        Person p1 = new Person();
+        System.out.println(p1.s);
+        p.s = 11;
+        System.out.println(Person.s);
     }
 }
 
